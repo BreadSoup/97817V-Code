ae58b59b4eb547e68add6907d0f577b284883454
DaxtonBingham <daxbing@gmail.com>
2025-02-12 17:18:11 -0500

    auto half field

--- .d/DriverControl/definitions.cpp.d
+++ .d/DriverControl/definitions.cpp.d
@@ -3,7 +3,8 @@ bin/DriverControl/definitions.cpp.o: src/DriverControl/definitions.cpp \
  include/pros/motor_group.hpp include/pros/abstract_motor.hpp \
  include/pros/device.hpp include/pros/misc.hpp include/pros/misc.h \
  include/pros/rtos.hpp include/pros/rtos.h include/pros/motors.h \
- include/pros/rtos.hpp include/pros/colors.hpp include/pros/motors.hpp
+ include/pros/rtos.hpp include/pros/colors.hpp include/pros/motors.hpp \
+ include/pros/rotation.hpp include/pros/rotation.h
 src/DriverControl/definitions.h:
 include/pros/adi.hpp:
 include/pros/adi.h:
@@ -18,3 +19,5 @@ include/pros/motors.h:
 include/pros/rtos.hpp:
 include/pros/colors.hpp:
 include/pros/motors.hpp:
+include/pros/rotation.hpp:
+include/pros/rotation.h:
--- src/DriverControl/definitions.h
+++ src/DriverControl/definitions.h
@@ -3,6 +3,7 @@
 
 #include "pros/adi.hpp"
 #include "pros/motor_group.hpp"
+#include "pros/rotation.hpp"
 
 extern pros::Controller master;
 extern pros::adi::DigitalOut piston;
@@ -14,7 +15,7 @@ extern pros::Motor lift;
 extern pros::Motor Redriect1;
 extern pros::Motor Redriect2;
 extern pros::MotorGroup Redriect;
-extern pros::adi::Encoder encoder;
+extern pros::Rotation encoder;
 
 
 extern bool pistonState;
